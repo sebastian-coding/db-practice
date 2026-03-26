618768c8ec165f32bde1d523909a5df6d898c465
Julian Medina <julian14medina05@gmail.com>
2026-03-25 20:40:23 -0500

    add sql file for inserting books data

---
+++ insert_books_data.sql
@@ -0,0 +1,9 @@
+insert into books (title, author_id,genre,price,publish_year)Values
+('Murder on the Orient Express',1,'Mystery',14.99,1934),
+('One Hundred Years of Solitude',2,'Magical Realism',19.99,1967),
+('The Shining',3,'Horror',15.50,1977),
+('Norwegian Wood',4,'Romance',12.00,1987),
+('Pride and Prejudice',5,'Romance',9.99,1813),
+('And Then There Were None',1,'Mystery',11.99,1939),
+('Love in the Time of Cholera',2,'Romance',16.50,1985),
+('It',3,'Horror',22.99,1986);
