d6ebea10800e786ee6a9bbf9b2f47ab43fc30053
Julian Medina <julian14medina05@gmail.com>
2026-03-25 20:19:40 -0500

    add sql file for inserting authors data

---
+++ insert_authors_data.sql
@@ -0,0 +1,5 @@
+insert into authors Values(1,'Agatha','Christie','UK'),
+(2,'Gabriel','Garcia Marquez','Colombia'),
+(3,'Stephen','King','USA'),
+(4,'Haruki','murakami','Japan'),
+(5,'Jane','Austen','UK');
